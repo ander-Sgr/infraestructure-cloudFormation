7da2fadd81f2ce6712f856f5e1fe9f30f2d95b11
ander <andersonbarreto2001@gmail.com>
2024-07-28 13:52:14 +0200

    script for deploy my templates to s3 bucket

---
+++ scripts/deploy_templates_s3.sh
@@ -0,0 +1,4 @@
+aws s3 cp templates/vpc-template.yaml s3://my-s3-ander-templates/
+aws s3 cp templates/subnets-template.yaml s3://my-s3-ander-templates/
+aws s3 cp templates/security-group-template.yaml s3://my-s3-ander-templates/
+aws s3 cp templates/ec2-instance-template.yaml s3://my-s3-ander-templates/
