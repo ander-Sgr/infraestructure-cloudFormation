00d03ffa6f0d7fbbbd42d6d0bc7d8e6b7076b70c
ander <andersonbarreto2001@gmail.com>
2024-07-29 00:16:39 +0200

    refactor script for upload the templates at the s3

--- scripts/deploy_templates_s3.sh
+++ scripts/deploy_templates_s3.sh
@@ -1,6 +1,71 @@
-aws s3 cp templates/vpc-template.yaml s3://my-s3-ander-templates/
-aws s3 cp templates/subnets-template.yaml s3://my-s3-ander-templates/
-aws s3 cp templates/alb-security-group-template.yaml s3://my-s3-ander-templates/
-aws s3 cp templates/ec2-security-template.yaml s3://my-s3-ander-templates/
-aws s3 cp templates/alb-template.yaml s3://my-s3-ander-templates/
-aws s3 cp templates/auto-escaling-group-template.yaml s3://my-s3-ander-templates/
+#!/bin/bash
+
+usage() {
+    echo "Usage: $0 -b BUCKET_NAME -r REGION -d TEMPLATE_DIR"
+    exit 1
+}
+
+# Parsear los argumentos
+while getopts ":b:r:d:" opt; do
+    case ${opt} in
+        b )
+            BUCKET_NAME=$OPTARG
+            ;;
+        r )
+            REGION=$OPTARG
+            ;;
+        d )
+            TEMPLATE_DIR=$OPTARG
+            ;;
+        \? )
+            usage
+            ;;
+    esac
+done
+
+if [ -z "$BUCKET_NAME" ] || [ -z "$REGION" ] || [ -z "$TEMPLATE_DIR" ]; then
+    usage
+fi
+
+# create the bucket
+if ! aws s3api head-bucket --bucket "$BUCKET_NAME" 2>/dev/null; then
+    echo "Creating bucket $BUCKET_NAME..."
+    aws s3api create-bucket --bucket "$BUCKET_NAME"
+else
+    echo "Bucket $BUCKET_NAME already exisits."
+fi
+
+echo "Disabling public access block..."
+aws s3api delete-public-access-block --bucket "$BUCKET_NAME"
+
+# policy for allow access to the resources
+POLICY=$(cat <<EOM
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "cloudformation.amazonaws.com"
+            },
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::$BUCKET_NAME/*"
+        },
+        {
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::$BUCKET_NAME/*"
+        }
+    ]
+}
+EOM
+)
+
+echo "Appling bucket policy..."
+aws s3api put-bucket-policy --bucket "$BUCKET_NAME" --policy "$POLICY"
+
+echo "Uploading templates..."
+aws s3 cp "$TEMPLATE_DIR" "s3://$BUCKET_NAME/" --recursive
+
+echo "All templates have been uploaded to $BUCKET_NAME."
