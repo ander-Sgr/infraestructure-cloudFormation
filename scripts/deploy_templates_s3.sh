3bfc1ecb7ef42ef8a3f73b8e0b45d32fdbb0f7c3
ander <andersonbarreto2001@gmail.com>
2024-07-28 19:38:06 +0200

    feat: adding script to upload the temaplates at my s3 bucket

--- scripts/deploy_templates_s3.sh
+++ scripts/deploy_templates_s3.sh
@@ -1,4 +1,6 @@
 aws s3 cp templates/vpc-template.yaml s3://my-s3-ander-templates/
 aws s3 cp templates/subnets-template.yaml s3://my-s3-ander-templates/
-aws s3 cp templates/security-group-template.yaml s3://my-s3-ander-templates/
-aws s3 cp templates/ec2-instance-template.yaml s3://my-s3-ander-templates/
+aws s3 cp templates/alb-security-group-template.yaml s3://my-s3-ander-templates/
+aws s3 cp templates/ec2-security-template.yaml s3://my-s3-ander-templates/
+aws s3 cp templates/alb-template.yaml s3://my-s3-ander-templates/
+aws s3 cp templates/auto-escaling-group-template.yaml s3://my-s3-ander-templates/
